8bbc4de4f87af7b697a317bec06946b8cfe061c2
Kunal Kapoor <kkapoor@ncsu.edu>
2016-10-29 13:46:20 -0400

    test

---
+++ aspell/gen/dirs.h
@@ -0,0 +1,4 @@
+#define PREFIX "/home/expertiza_developer/heroku-buildpack-aspell/aspell"
+#define DATA_DIR "<prefix:lib/aspell-0.60>"
+#define DICT_DIR "<data-dir>"
+#define CONF_DIR "<prefix:etc>"
